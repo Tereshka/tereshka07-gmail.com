6f11cd6be8f20ce58bd07baa17423a025dba8909
Tereshka <tereshka07@gmail.com>
2020-03-25 18:03:36 +0900

    Updates

---
+++ precache-manifest.83d36b7adccf234574b24c0f4a02dba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3893382d81daab4dad30f36940dd58e8",
+    "url": "/pic-some-app/index.html"
+  },
+  {
+    "revision": "c3834115b4c8c5210fcb",
+    "url": "/pic-some-app/static/css/main.67bce4ac.chunk.css"
+  },
+  {
+    "revision": "19e5b9bf130b9c3c1a09",
+    "url": "/pic-some-app/static/js/2.fe681f93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pic-some-app/static/js/2.fe681f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3834115b4c8c5210fcb",
+    "url": "/pic-some-app/static/js/main.3da3e90b.chunk.js"
+  },
+  {
+    "revision": "6b2dc856efebe81a47b4",
+    "url": "/pic-some-app/static/js/runtime-main.e5aa8f63.js"
+  }
+]);
